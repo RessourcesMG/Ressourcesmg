1be6a4fc1eedf980e08c93489ac4d55db17c23ab
RessourcesMG <alouettev@gmail.com>
2026-02-12 21:46:11 +0100

    Replace Vercel Analytics component with inject method for script loading in main application

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Analytics } from '@vercel/analytics/react'
+import { inject } from '@vercel/analytics'
 import './index.css'
 import App from './App.tsx'
 import { Webmaster } from './pages/Webmaster.tsx'
+
+// Injecte le script Analytics au chargement
+inject()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,7 +16,6 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path="/webmaster" element={<Webmaster />} />
       </Routes>
-      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 )
