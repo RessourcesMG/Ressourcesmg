9e2e68a5ae2fbfa5c690f97c287f7d9b2d009684
RessourcesMG <alouettev@gmail.com>
2026-02-12 20:17:03 +0100

    Add Vercel Analytics component to main application render

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
 import { Webmaster } from './pages/Webmaster.tsx'
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path="/webmaster" element={<Webmaster />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 )
